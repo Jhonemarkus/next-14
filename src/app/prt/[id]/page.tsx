d433d5b310e1dc4e5746b4939040613e68c8683f
Jhonemarkus <jmsmoro@gmail.com>
2024-03-05 13:52:34 -0500

    editing a todo (#4)
    * editing a todo
    
    * type fixes
    
    ---------
    
    Co-authored-by: Marcos Moro <marcos.m@myplanet.com>

--- src/app/prt/[id]/page.tsx
+++ src/app/prt/[id]/page.tsx
@@ -1,7 +1,11 @@
 "use client"
 
 import TODOForm from "@/components/todoForm"
+import useTODOList from "@/hooks/useTODOList"
 import { useRouter } from "next/navigation"
+import { useMemo } from "react"
+import { useFormState } from "react-dom"
+import { ITodo } from "../types/iTodo"
 
 type IEditTODOParams = {
   params: {
@@ -10,11 +14,34 @@ type IEditTODOParams = {
 }
 
 export default function EditTODO({params}: IEditTODOParams) {
+  const router = useRouter()
   const {id} = params
+  const {data, isLoading, update} = useTODOList()
+  const todo = useMemo(() => {
+    return (data || []).find(el => el.id === id)
+  }, [data, id])
   
+  const [formState, formAction] = useFormState(async (prevState: any, formData: FormData) => {
+    const todo: ITodo = {
+      id: `${id}`,
+      title: `${formData.get('title')}`,
+      description: `${formData.get('description')}`
+    }
+    const todoIndex = data.findIndex(el => el.id === id)
+    const todoList = [...data]
+    todoList[todoIndex] = todo
+    update(todoList)
+    router.push('/prt')
+  }, { todo })
+
+  if (isLoading || todo == null) {
+    return <div>...loading</div>
+  }
+
+
   return (
     <>
-      <TODOForm action={"#"}/>
+      <TODOForm action={formAction} todo={todo}/>
     </>
   )
 }
