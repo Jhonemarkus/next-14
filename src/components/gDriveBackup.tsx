02d6789aa4b259081b4f7d13749e0eb1b4027c4b
Marcos Moro <marcos.m@myplanet.com>
2024-06-06 14:14:24 -0400

    listing files

--- src/components/gDriveBackup.tsx
+++ src/components/gDriveBackup.tsx
@@ -1,9 +1,11 @@
 import Script from "next/script"
 import { useGDrive } from "./prtProvider"
+import { useState } from "react"
 
 export default function GDriveBackup() {
+  const [accessTokenReady, setAccessTokenReady] = useState<boolean>(false)
   const { onLoadGAPI, onLoadGis, isLoading, tokenClient } = useGDrive()
-  if (!isLoading) {
+  if (!isLoading && ! accessTokenReady) {
     const token = window.gapi.client.getToken()
     console.log('gdrive token', { token })
     // TODO: check if already authorized
@@ -11,12 +13,21 @@ export default function GDriveBackup() {
       // TODO: authorize user
       tokenClient.callback = (resp) => {
         console.log('token Client callback', { resp })
+        setAccessTokenReady(true)
       }
       tokenClient.requestAccessToken({prompt: 'consent'});
     } else {
-      // tokenClient.requestAccessToken({prompt: ''});
+      tokenClient.requestAccessToken({prompt: ''});
     }
+  }
+  if (accessTokenReady) {
     // TODO: list files
+    gapi.client.drive.files.list({
+      'pageSize': 10,
+      'fields': 'files(id, name)',
+    }).then((response) => {
+      console.log('response', {response})
+    });
     // TODO: change scope to appdata
     // TODO: check if data exist in GDrive
     // TODO: compare which one is newer (both must have backup enabled)
