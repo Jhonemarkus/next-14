faf852a4862bea9d37399db2349076886d089a54
Jhonemarkus <jmsmoro@gmail.com>
2024-07-24 11:13:59 -0400

    Issue/24 primary btn (#27)
    * Adding primary button
    
    * using correct property name
    
    ---------
    
    Co-authored-by: Marcos Moro <marcos.m@myplanet.com>

---
+++ src/components/primaryButton.tsx
@@ -0,0 +1,17 @@
+import { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export default function PrimaryButton({ children, ...props}: ButtonProps) {
+  return (
+    <button
+      type="button"
+      className="border-2 border-black rounded bg-black text-white px-4 py-2"
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
