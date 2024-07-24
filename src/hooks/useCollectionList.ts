14073847c4f2720cae078f3e69c04c57e9dbc081
Marcos Moro <marcos.m@myplanet.com>
2024-07-24 11:51:30 -0400

    returning collection hook on provider

--- src/hooks/useCollectionList.ts
+++ src/hooks/useCollectionList.ts
@@ -1,16 +1,10 @@
-import { useCallback, useReducer } from "react"
+import { useReducer } from "react"
 import { reducerCollectionList } from "./reducers/reducerCollectionList"
-import { Collection } from "@/types/Collection"
-import { CollectionListState } from "@/types/hooks/CollectionListState"
 
 export default  function useCollectionList () {
   const [state, dispatch] = useReducer(reducerCollectionList, {
     collectionList: []
   })
-
-  const createCollection = useCallback((collection: Collection) => {
-    console.info("on mount")
-  }, [])
   
   return {
     state,
--- src/providers/collectionProvider.tsx
+++ src/providers/collectionProvider.tsx
@@ -1,13 +1,14 @@
 'use client'
 
+import useCollectionList from "@/hooks/useCollectionList"
 import { createContext } from "react"
 
 export const CollectionContext = createContext(null)
 
 export default function CollectionProvider ({ children }) {
-
+  const useCollectionListValue = useCollectionList()
   return (
-    <CollectionContext.Provider value="my value">
+    <CollectionContext.Provider value={{useCollectionList: useCollectionListValue}}>
       {children}
     </CollectionContext.Provider>
   )
